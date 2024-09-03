df90b6e8774ac6da03cbff84736e8f2a48a99b09
Sergiu Plesco <plescosergiu@gmail.com>
2024-09-03 20:19:46 +0300

    new folder structure

---
+++ rest-api/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/SergiuPlesco/microservices-go/rest-api/handlers"
+)
+
+func main() {
+	l := log.New(os.Stdout, "product-api", log.LstdFlags)
+
+	// create the handlers
+	hh := handlers.NewHello(l)
+	gh := handlers.NewGoodbye(l)
+
+	// create a new serve mux and register the handlers
+	sm := http.NewServeMux()
+	sm.Handle("/", hh)
+	sm.Handle("/goodbye", gh)
+
+	// create new server
+	s := &http.Server{
+		Addr:         ":9100",           // configure the bind address
+		Handler:      sm,                //set the default handler
+		ErrorLog:     l,                 // set the logger for the server
+		IdleTimeout:  120 * time.Second, // max time to read request from the client
+		ReadTimeout:  1 * time.Second,   // max time to write response to the client
+		WriteTimeout: 1 * time.Second,   // max time for connections using TCP Keep-Alive
+	}
+
+	// start the server
+	go func() {
+		log.Println("Starting server on :9100...")
+		err := s.ListenAndServe()
+		if err != nil {
+			l.Fatal(err)
+		}
+	}()
+	// trap sigterm or interrupt and gracefully shutdown the server
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, os.Interrupt)
+
+	sig := <-sigChan
+	l.Println("Recieved tarminate, graceful shutdown", sig)
+
+	tc, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	log.Println("Shutting down server...")
+	s.Shutdown(tc)
+
+	log.Println("Server exited properly")
+}
